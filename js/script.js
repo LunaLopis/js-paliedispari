23ddfc69b4c89cd26d0aa904d6f88935235dc18b
Luna Lopis <lunalopis59@gmail.com>
2023-05-27 17:30:30 +0200

    primo  esericizio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-
+function checkPalindroma(parola) {
+    let palindroma = parola.split('').reverse().join('');
+    console.log(palindroma);
+    console.log(parola);
+    if (palindroma.toLowerCase() === parola.toLowerCase()) {
+      return true;
+    }
+    return false;
+  }
   
+  let word = prompt('Inserire una parola');
+  let check = checkPalindroma(word);
+  if (check) {
+    console.log('La parola è palindroma');
+  } else {
+    console.log('La parola non è palindroma');
+  }
 
-
+//   tolowercase perchè se la parola inizia con la maiuscola, js non la riconosce come palindroma
